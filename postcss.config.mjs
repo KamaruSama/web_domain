8e401b4feb16f2070c4a4f37c0bcb521f2166683
Kamaru_Sama <k.kamarux@gmail.com>
2025-07-04 17:49:25 +0700

    s

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,6 @@
-const config = {
-  plugins: ["@tailwindcss/postcss"],
+// postcss.config.mjs
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},   // ให้ PostCSS เรียก Tailwind engine
+  },
 };
-
-export default config;
